b846d6169c50c25db29b6e567550b5048fe4bc54
T3Nero <ryan-16@hotmail.co.uk>
2024-05-14 16:06:00 +0100

    Created Character Animation Blueprints
    Aura Animation Blueprint
    Enemy Parent Animation Blueprint
    Goblin Spear Animation Blueprint (Child of Enemy ABP)

--- Source/Decim8/Private/Character/Decim8CharacterBase.cpp
+++ Source/Decim8/Private/Character/Decim8CharacterBase.cpp
@@ -9,6 +9,10 @@ ADecim8CharacterBase::ADecim8CharacterBase()
 
 	PrimaryActorTick.bCanEverTick = false;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
 }
 
 // Called when the game starts or when spawned
