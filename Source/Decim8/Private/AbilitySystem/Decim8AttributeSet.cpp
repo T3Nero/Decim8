1e3fef230d4f62a1fbf7c50b160bf4b5139a59ad
T3Nero <ryan-16@hotmail.co.uk>
2024-06-16 12:29:01 +0100

    Started adding attribute data to the attribute set
    Attribute Set contains all the stats for the player & npcs (Health, Mana etc.)

--- Source/Decim8/Private/AbilitySystem/Decim8AttributeSet.cpp
+++ Source/Decim8/Private/AbilitySystem/Decim8AttributeSet.cpp
@@ -3,3 +3,41 @@
 
 #include "AbilitySystem/Decim8AttributeSet.h"
 
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
+
+UDecim8AttributeSet::UDecim8AttributeSet()
+{
+}
+
+void UDecim8AttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	// Required when using OnRep to replicate attributes across the network
+	DOREPLIFETIME_CONDITION_NOTIFY(UDecim8AttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UDecim8AttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UDecim8AttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UDecim8AttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UDecim8AttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDecim8AttributeSet, Health, OldHealth);
+}
+
+void UDecim8AttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDecim8AttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UDecim8AttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDecim8AttributeSet, Mana, OldMana);
+}
+
+void UDecim8AttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDecim8AttributeSet, MaxMana, OldMaxMana);
+}
--- Source/Decim8/Public/AbilitySystem/Decim8AttributeSet.h
+++ Source/Decim8/Public/AbilitySystem/Decim8AttributeSet.h
@@ -13,5 +13,39 @@ UCLASS()
 class DECIM8_API UDecim8AttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+
+	UDecim8AttributeSet();
+
+	// Must be overriden when using replication for multiplayer games
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty> &OutLifetimeProps) const override;
+
+	// Setup attribute data to handle base/current stat values
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Primary Attributes")
+	FGameplayAttributeData Health;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Primary Attributes")
+	FGameplayAttributeData MaxHealth;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Primary Attributes")
+	FGameplayAttributeData Mana;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category = "Primary Attributes")
+	FGameplayAttributeData MaxMana;
+
+
+	// Required in multiplayer games to replicate attribute data across server/clients
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData &OldHealth) const;
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
 	
 };
